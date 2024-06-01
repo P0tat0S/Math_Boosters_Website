8b9ffc1e4ab24d42e6bd05e99160f868941f590f
Mark William Fernandez <57150058+P0tat0S@users.noreply.github.com>
2024-06-01 21:19:11 +0100

    Init Commit, add base template UI Materails

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LandingPage from '../src/landing-page/LandingPage'; // Make sure the path is correct
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
